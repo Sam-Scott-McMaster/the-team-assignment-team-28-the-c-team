fb24881dfd54e357a20890bb43b8f120d3b677ca
TalhaAtif <talha.atif.89@gmail.com>
2024-12-04 00:49:29 -0500

    Added comments

--- GUI_inputs.c
+++ GUI_inputs.c
@@ -1,8 +1,14 @@
+/* (Muhammad Talha Atif), (atifm8), (December 5, 2024)
+*
+* File contains all handling and function calling based
+* on user inputs after a GUI menu is printed
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+//Includes all methods creating by group
 #include "GUI.h"
 #include "GUI_inputs.h"
 #include "users.h"
@@ -10,14 +16,22 @@
 #include "transaction_helpers.h"
 #include "sortHistory.h"
 
+/* action
+* takes in username and user balance
+* is method called to accept input from homepage,
+* displays next terminal menu and calls next user input
+* function
+* includes clean input handling for invalid inputs
+*/
 void action(char* user, long int balance) {
-    homePage(user, balance);
+    homePage(user, balance); //Prints homepage
     printf("\n\n");
     int input;
     int inputCheck;
-    int c;
-    while(1) {
-        inputCheck = scanf("%d", &input);
+    int c; //int to erase current input
+    while(1) { 
+        inputCheck = scanf("%d", &input); //Takes input
+        //Checks if input is a valid options
         if (inputCheck && input < 4 && input > 0 || input == 1738) {
             if (input == 1) {
                 loading();
@@ -34,21 +48,22 @@ void action(char* user, long int balance) {
             else if (input == 3) {
                 exit(0);
             }
-            else if (input == 1738) { //Testing code
-                loading();
-                return;
-            }
         }
+        //Erases input and asks for new input
         else {
             printf("\033[1A");
             printf("\033[2K");
             printf("Invalid input, try again: ");
+            //Erases current input to avoid loop
             while ((c = getchar()) != '\n' && c != EOF) { }
         }
     }
 }
 
-
+/* budget
+* handles user input for budget menu, calling appropriate methods
+* also has help option
+*/
 void budget(char* user,long int balance) {
     printf("\n\n");
     int input;
@@ -56,6 +71,7 @@ void budget(char* user,long int balance) {
     int c;
     while(1) {
         inputCheck = scanf("%d", &input);
+        //Checks if input is valid
         if (inputCheck && input < 5 && input > 0) {
             if (input == 1) {
                 addBudget(user);
@@ -73,19 +89,21 @@ void budget(char* user,long int balance) {
 
             }
         }
+        //Otherwise performs clean user input handling
         else {
             printf("\033[1A");
             printf("\033[2K");
             printf("Invalid input, try again: ");
+            //Erases current input
             while ((c = getchar()) != '\n' && c != EOF) { }
         }
-        
-        while ((c = getchar()) != '\n' && c != EOF) { }
-        budgetMenu(user);
-
     }
 }
 
+/* transaction
+* takes in username and user balance
+* handles invalid user input same way as previous functions
+*/
 void transaction(char* user, long int balance) {
     printf("\n\n");
     int input;
@@ -98,6 +116,7 @@ void transaction(char* user, long int balance) {
                 addTransaction(user);
             }
             else if (input == 2) {
+                //Calls methods to print sorted transactions
                 sortHistory("dataBase.txt", "sortedTrans.txt");
                 printFileToTerminal("sortedTrans.txt");
             }
@@ -107,6 +126,7 @@ void transaction(char* user, long int balance) {
             else if (input == 4) {
                 action(user,balance);
             }
+            //Help method
             else if (input == 5) {
                 printf("The addTransaction() function lets users input details for a new financial transaction.\n");
                 printf("Users specify the date, category (e.g., food, entertainment), amount, and optional notes.\n");
@@ -115,18 +135,19 @@ void transaction(char* user, long int balance) {
             }
         }
         else {
+            //Clean invalid input handling
             printf("\033[1A");
             printf("\033[2K");
             printf("Invalid input, try again: ");
             while ((c = getchar()) != '\n' && c != EOF) { }
         }
-
-        while ((c = getchar()) != '\n' && c != EOF) { }
-       transactionMenu(user);
-
     }
 }
 
+/* loading
+* prints out dyanmic "animated" loading animation
+* in order to immerse user in app experince
+*/
 void  loading() {
     for (int i = 1; i < 11; i++) {
         printf("\r");
@@ -141,6 +162,11 @@ void  loading() {
     printf("\n");
 }
 
+/* signIn_or_Up
+* handles input for initial start-up for application
+* forcing user to either create an account or sign in
+* also allows user to exit app
+*/
 void signIn_or_Up(char* user) {
     printf("\n\n");
     int input;
@@ -162,7 +188,7 @@ void signIn_or_Up(char* user) {
             else if (input == 3) {
                 exit(0);
             }
-            else if (input == 4) { //Testing code
+            else if (input == 4) { //Provides user help method
                 printf("The signin/up() function allows users to either sign in to their existing SpendFlow account or create a new account.\n");
                 printf("Prompted to enter 1, 2, or 3 to either sign in, sign up, or exit.\n");
                 printf("Signing in retrieves the user’s saved data, including past transactions and budgets.\n");
@@ -171,6 +197,7 @@ void signIn_or_Up(char* user) {
             }
         }
         else {
+            //Clean and simple invalid input handling
             printf("\033[1A");
             printf("\033[2K");
             printf("Invalid input, try again: ");
@@ -179,6 +206,11 @@ void signIn_or_Up(char* user) {
     }
 }
 
+/* viewBudget
+* simple method to return users budget, handles in case 
+* more than one budget has been created (modular in case
+* program is expanded in the future)
+*/
 void viewBudget(char* user) {
     FILE *fileOpen = fopen(allBud(user), "r+");
 
